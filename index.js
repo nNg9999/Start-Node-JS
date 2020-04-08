ec0d324f2fb9e6c237974a8ef702b4ccf06ce7d9
nNg9999 <n_n_g_@ukr.net>
2020-04-08 22:04:04 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Webpack");
+
+import { messege } from "./messege";
+console.log(messege);
+
+import "./style.css";
+// import css from "file.css";
+import listItemTpl from "./list-item.hbs";
+console.log(listItemTpl);
---
+++ messege.js
@@ -0,0 +1,13 @@
+"use strict";
+export const messege = () => "Ytllo node JS";
+
+// module.exports = {
+//   messege
+// };
+
+// =======================================
+// export default messege;
+// =======================================
+
+export const AsArctical = () => console.log("Is artical");
+export const autsUser = () => console.log("autsUser");
